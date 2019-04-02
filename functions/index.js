2e1fa82bf928ff17a25e56963b15fbc7f3f0e055
nish17 <nimesh3536@gmail.com>
2019-04-02 18:20:31 +0530

    update the code with proper firebase cloud storage config

--- functions/index.js
+++ functions/index.js
@@ -1,26 +1,25 @@
 "use strict";
 const { dialogflow } = require("actions-on-google");
-const functions = require("firebase-functions");
-const config = require("./config.js");
 const app = dialogflow();
-var firebase = require("firebase-admin");
-
-firebase.initializeApp(config);
-let storage = firebase.storage();
 let Path = "";
-app.intent("Default Welcome Intent", conv => {
-    let intentName = "Default_Welcome_Intent";
 
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+admin.initializeApp(functions.config().firebase);
+const bucket = admin.storage().bucket();
+const bucketAccessConfig = {
+    action: "read",
+    expires: Date.now() + 1000 * 60 * 60
+};
+
+app.intent("Default Welcome Intent", async conv => {
+    let intentName = "Default_Welcome_Intent";
     Path = `${intentName}/${Math.floor(Math.random() * 4) + 1}.ogg`;
-    return storage
-        .child(Path)
-        .getDownloadURL()
-        .then(url => conv.ask(`<speak><audio src=${url}/></speak>`))
-        .catch(e => {
-            console.log("````````````````````````````````````````````````````");
-            console.log(e);
-            console.log("````````````````````````````````````````````````````");
-        });
+    const soundFileRef = bucket.file(Path);
+    const [soundUrl] = await soundFileRef.getSignedUrl(bucketAccessConfig);
+    // console.log(Path);
+    // console.log(storage);
+    conv.ask(`<speak><audio src=${soundUrl}/></speak>`);
 });
 
 app.intent("Default Fallback Intent", conv => {
